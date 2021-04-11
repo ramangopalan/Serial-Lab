497272b415813e50ad2a2bd85a6a5712ec875edc
Raman Gopalan <ramangopalan@gmail.com>
2021-04-11 11:51:16 +0530

    Rename: Cooing power -> Heating power

--- Serial Lab/Form1.Designer.cs
+++ Serial Lab/Form1.Designer.cs
@@ -1003,9 +1003,9 @@
             this.label5.AutoSize = true;
             this.label5.Location = new System.Drawing.Point(7, 103);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(123, 17);
+            this.label5.Size = new System.Drawing.Size(125, 17);
             this.label5.TabIndex = 30;
-            this.label5.Text = "Cooling power (%)";
+            this.label5.Text = "Heating power (%)";
             this.label5.Click += new System.EventHandler(this.label5_Click);
             // 
             // power_percent
